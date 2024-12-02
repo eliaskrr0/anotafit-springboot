e0450fd6edaf3e4fb76f3268514f18c94d56e81b
Ilyasse <developer.elias.essadak@gmail.com>
2024-12-02 21:20:54 +0100

    inicio

---
+++ src/main/java/es/anotafit/AnotafitApplication.java
@@ -0,0 +1,13 @@
+package es.anotafit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotafitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotafitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/anotafit/AnotafitApplicationTests.java
@@ -0,0 +1,13 @@
+package es.anotafit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnotafitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
